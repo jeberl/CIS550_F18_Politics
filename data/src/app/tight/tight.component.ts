4687176466395e654d73a9c80845c710d8949301
nprofit97 <nprofit97@gmail.com>
2018-11-18 13:49:39 -0500

    Add files via upload
    adding source folder for template project

---
+++ data/src/app/tight/tight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tight',
+  templateUrl: './tight.component.html',
+  styleUrls: ['./tight.component.css']
+})
+export class TightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
