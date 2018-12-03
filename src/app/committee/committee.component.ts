a3089b30433b93d7a6681146bf66a24111f9288b
nprofit97 <nprofit97@gmail.com>
2018-12-03 18:19:49 -0500

    Add very basic dropdowns

--- src/app/committee/committee.component.ts
+++ src/app/committee/committee.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./committee.component.css']
 })
 export class CommitteeComponent implements OnInit {
+ 
+  committees = ["Agriculture", "Finance", "Schools", "Idk", "Just A Test"];
+  subs = ["super", "specific", "subcommittee"];
 
   constructor() { }
 
   ngOnInit() {
+  	// should populate committee dropdown upon page load
+  }
+
+  // Runs when user selects a committee
+  selectCommittee(committee) {
+  	this.getSubs(committee);
+  }
+
+  // Gets Subcommittees associated with current committee
+  getSubs(committee) {
+  	// call backend method
+  	// subs = output of SQL query
+  }
+
+  // Gets members associated with current subcommittee
+  getMembers(subcommittee) {
+  	// call backend method
+  	// members = output of SQL query
   }
 
 }
