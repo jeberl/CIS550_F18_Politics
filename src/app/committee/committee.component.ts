043122dec842bcdeca215c81bdb48bf75fbd4b2d
nprofit97 <nprofit97@gmail.com>
2018-12-06 18:38:43 -0500

    Comment out javascript errors for now

--- src/app/committee/committee.component.ts
+++ src/app/committee/committee.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+// import * as Backend:any from "../backend.js";
+// import * as Backend from "../backend.js";
+
 
 @Component({
   selector: 'app-committee',
   templateUrl: './committee.component.html',
   styleUrls: ['./committee.component.css']
 })
+
+
 export class CommitteeComponent implements OnInit {
  
+  //import Backend = require("../backend.js");
   committees = ["Agriculture", "Finance", "Schools", "Idk", "Just A Test"];
   subs = ["super", "specific", "subcommittee"];
-
-  constructor() { }
+  // constructor() { }
 
   ngOnInit() {
   	// should populate committee dropdown upon page load
@@ -24,6 +29,7 @@ export class CommitteeComponent implements OnInit {
   // Gets Subcommittees associated with current committee
   getSubs(committee) {
   	// call backend method
+    // this.b.get_all_subcomittees_on_committee(committee);
   	// subs = output of SQL query
   }
 
